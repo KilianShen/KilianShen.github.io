073093f6aa0246c3437faa4d140b975235930ba6
shenqingli <shenqingli@apexsoft.com.cn>
2023-04-16 23:10:48 +0800

    test7

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  base:'VitePress-static-web',
   title: "SQLnote",
   description: "A VitePress Site",
   lastUpdated:true,
